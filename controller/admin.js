1810f63ca7987df7d331bd7d620b80ecc16cae98
kwak123 <samuel_kwak@apple.com>
2018-10-13 13:24:33 -0400

    fix controller lint

--- controller/admin.js
+++ controller/admin.js
@@ -2,22 +2,20 @@ const db = require('../db');
 const KEY = require('../env/keys').PRIVILEGED_ACCESS;
 
 const updateDB = (req, res) => {
-  let sub = req.query.sub;
-  let key = req.query.key;
+  const { sub, key } = req.query;
   if (key === KEY) {
     db.updateSchedule(sub)
-    .then((result) => {
-      res.sendStatus(200);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.sendStatus(404);
-    })
+      .then(() => res.sendStatus(200))
+      .catch((error) => {
+        /* eslint-disable-next-line no-console */
+        console.log(error);
+        return res.sendStatus(404);
+      });
   } else {
     res.sendStatus(403);
   }
 };
 
 module.exports = {
-  updateDB
+  updateDB,
 };
