943ca4f2961942c3ddd4c3cbf79cd76651f14e16
Samuel Kwak <kwak123@gmail.com>
2018-10-13 13:18:03 -0400

    fix db lint errors

--- db/config.js
+++ db/config.js
@@ -3,15 +3,17 @@ const keys = require('../env/keys');
 
 const connection = mysql.createConnection({
   host: keys.DB_HOST,
-  port: keys.DB_PORT,  
+  port: keys.DB_PORT,
   database: keys.DB_NAME, // Change this
   user: keys.DB_USER,
-  password: keys.DB_PASSWORD
+  password: keys.DB_PASSWORD,
 });
 
 connection.connect((error) => {
+  /* eslint-disable-next-line no-console */
   if (error) { return console.log(error); }
-  console.log(`connected with id ${connection.threadId}`);
+  /* eslint-disable-next-line no-console */
+  return console.log(`connected with id ${connection.threadId}`);
 });
 
 module.exports = connection;
