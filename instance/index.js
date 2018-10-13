d5bd63fe875eecb62755642609861bc54af1d3fb
Samuel Kwak <kwak123@gmail.com>
2018-10-13 13:27:53 -0400

    fix instance lint

--- instance/index.js
+++ instance/index.js
@@ -1,35 +1,32 @@
-const { setTimout } = require('timers');
+// const { setTimeout } = require('timers');
 
 // Ideally, you'd add parsers here to expand data requests
 // They need to be promises for the sake of service scheduler
 const mtaServiceFetcher = require('../parsers').mta.serviceParser.fetchServiceStatus;
 
 // Keys and fetchers need to maintain a 1 to 1 ratio to maintain data organization
-const SUBS = ['mta']
+const SUBS = ['mta'];
 
 const _instance = { service: {} };
-let initialized = false;
-
-
+// let initialized = false;
 
 /* Service data */
 const _serviceScheduler = () => {
   // Invoke fetcher promises here
-  initialized = true;
+  // initialized = true;
   Promise.all([mtaServiceFetcher()])
-  .then((results) => {
-    results.forEach((el, i) => {
-      _instance.service[SUBS[i]] = el;
-    });
-    setTimeout(_serviceScheduler, 1000 * 60 * 5);
-  })
-  .catch((error) => console.log(error));
+    .then((results) => {
+      results.forEach((el, i) => {
+        _instance.service[SUBS[i]] = el;
+      });
+      setTimeout(_serviceScheduler, 1000 * 60 * 5);
+    })
+    /* eslint-disable-next-line no-console */
+    .catch(error => console.log(error));
 };
 
 // Start the fetcher
 _serviceScheduler();
-
-
 
 /* Getters */
 
@@ -40,14 +37,14 @@ const getServiceData = (sub) => {
 
 const getServiceRouteData = (sub, routeId) => {
   if (!_instance.service) { return false; }
-  return _instance.service[sub].lines.find((a) => a.name.includes(routeId));
+  return _instance.service[sub].lines.find(line => line.name.includes(routeId));
 };
 
-/** 
+/**
  * Not sure what this does just yet
  */
 
 module.exports = {
   getServiceData,
-  getServiceRouteData
+  getServiceRouteData,
 };
